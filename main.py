37d5c75de38bb7380f7004050981a0c2ca73d611
mariam <mariam.zaki502@gmail.com>
2025-04-01 08:52:43 +0200

    update main

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI 
-app=FastAPI()
+#from fastapi import FastAPI 
+#app=FastAPI()
 #calling function through api 
 #run as app server using uvicorn
-#python -m uvicorn main:app --reload     url/welcome
-@app.get("/welcome")
-def welcome():
-    return {
-        "massage":"hello world"
-    }
+#python -m uvicorn main:app --reload     url/"welcome"
+#test in swagger or postman
+#@app.get("/")
+#def welcome():
+#    return {
+#        "massage":"hello world"
+#   }
+
+####manage main with base.py
+#dotenv is a Python package that helps manage environment variables 
+# by loading them from a .env file into your application's environment. 
+# This is useful for keeping sensitive information (like API keys, database credentials, and secret tokens) 
+# out of your code.
+from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
+app = FastAPI()
+
+app.include_router(base.base_router)
 
  
---
+++ routes/__init__.py
@@ -0,0 +1,2 @@
+#We have a lot of python files, 
+# so it's best to start with init to familiarize ourselves with the moudule structure. 
